b6b2ae4f50b718a27376d547e8b130ea0da3b055
Mohamed salah naija <naija963@gmail.com>
2025-11-17 21:07:02 +0100

    feat: back to top btn

--- src/components/ui/back-to-top.tsx
+++ src/components/ui/back-to-top.tsx
@@ -45,6 +45,9 @@ export const BackToTop = (): JSX.Element | null => {
       aria-label="Back to top"
     >
       <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
         strokeWidth={2}
